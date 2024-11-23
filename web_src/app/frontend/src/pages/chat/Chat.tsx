42ca1691333ab1ce7cc847002462651f19e8aa07
Duong Nguy <duongnt.cnthongtin@gmail.com>
2024-11-23 17:17:09 +0700

    fix logo url

--- web_src/app/frontend/src/pages/chat/Chat.tsx
+++ web_src/app/frontend/src/pages/chat/Chat.tsx
@@ -371,7 +371,7 @@ const Chat = () => {
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
                             {/* <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" /> */}
-                            <img src="../../android-chrome-192x192.png" alt="Chat logo" style={{ width: "120px", height: "120px" }} />
+                            <img src="https://st7kgzeykxb2u3k.blob.core.windows.net/content/android-chrome-192x192.png?sp=r&st=2024-11-23T10:11:14Z&se=2024-11-29T18:11:14Z&skoid=89ecba64-d5b4-4fdc-88eb-a73bb52d2853&sktid=e3875a22-14d3-470f-b496-1908a4006022&skt=2024-11-23T10:11:14Z&ske=2024-11-29T18:11:14Z&sks=b&skv=2022-11-02&sv=2022-11-02&sr=b&sig=tJgHIBXyJ4gpH420zfxslOx8srcpPsTKwxPGGI7Qt84%3D" alt="Chat logo" style={{ width: "120px", height: "120px" }} />
                             <h1 className={styles.chatEmptyStateTitle}>{t("chatEmptyStateTitle")}</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>{t("chatEmptyStateSubtitle")}</h2>
                             {/* {showLanguagePicker && <LanguagePicker onLanguageChange={newLang => i18n.changeLanguage(newLang)} />} */}
